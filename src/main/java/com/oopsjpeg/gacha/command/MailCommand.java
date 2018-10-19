8352f06df9ec0d0341ccf6a89e60fbd6b8cde882
oopsjpeg <jvwill00@gmail.com>
2018-10-19 16:51:04 -0400

    Add mail discarding

--- src/main/java/com/oopsjpeg/gacha/command/MailCommand.java
+++ src/main/java/com/oopsjpeg/gacha/command/MailCommand.java
@@ -13,6 +13,9 @@ import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.handle.obj.IUser;
 import sx.blah.discord.util.EmbedBuilder;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MailCommand implements Command {
 	private final Gacha instance = Gacha.getInstance();
 
@@ -25,7 +28,22 @@ public class MailCommand implements Command {
 		if (info.getMail().isEmpty())
 			// User doesn't have any mail
 			Util.sendError(channel, author, "you do not have any mail.");
-		else if (args.length >= 1 && args[0].equalsIgnoreCase("collect")) {
+		else if (args.length >= 1 && args[0].equalsIgnoreCase("discard")) {
+			// Discard mail
+			if (args.length >= 2 && args[1].equalsIgnoreCase("all"))
+				// Discard all deletable mail
+				discardAll(channel, info);
+			else if (args.length >= 2) {
+				// Discard specific mail
+				int index = Integer.parseInt(args[1]);
+				if (index <= 0 || index > info.getMail().size())
+					Util.sendError(channel, author, "invalid mail index.");
+				else
+					discard(channel, info, info.getMail(index - 1));
+			} else if (info.getLastMail() != null)
+				// Discord last mail viewed
+				discard(channel, info, info.getLastMail());
+		} else if (args.length >= 1 && args[0].equalsIgnoreCase("collect")) {
 			// Collect gifts from mail
 			if (args.length >= 2 && args[1].equalsIgnoreCase("all"))
 				// Collect all gifts available
@@ -37,12 +55,9 @@ public class MailCommand implements Command {
 					Util.sendError(channel, author, "invalid mail index.");
 				else
 					collectGift(channel, info, info.getMail(index - 1));
-			} else if (info.getLastMail() != null && info.getLastMail().hasGift()
-					&& !info.getLastMail().isGiftCollected())
+			} else if (info.getLastMail() != null)
 				// Collect gift from last mail viewed
 				collectGift(channel, info, info.getLastMail());
-			else // Default to collecting all gifts
-				collectAllGifts(channel, info);
 		} else {
 			// View specific mail / most recent mail
 			int index = args.length == 0 ? info.getMail().size() : Integer.parseInt(args[0]);
@@ -50,8 +65,10 @@ public class MailCommand implements Command {
 				Util.sendError(channel, author, "invalid mail index.");
 			else {
 				UserMail mail = info.getMail(index - 1);
+				instance.getMongo().saveUser(info);
 				Bufferer.sendMessage(channel, "Viewing mail **" + index + "** of **"
-						+ info.getMail().size() + "** for " + Util.nameThenID(author) + ".", embed(mail));
+								+ info.getMail().size() + "** for " + Util.nameThenID(author) + ".",
+						embed(author, channel, mail));
 			}
 		}
 	}
@@ -66,7 +83,7 @@ public class MailCommand implements Command {
 		return true;
 	}
 
-	private EmbedObject embed(UserMail mail) {
+	private EmbedObject embed(IUser user, IChannel channel, UserMail mail) {
 		IUser mailAuthor = mail.getContent().getAuthor();
 
 		EmbedBuilder builder = new EmbedBuilder();
@@ -74,8 +91,9 @@ public class MailCommand implements Command {
 		builder.withAuthorIcon(mailAuthor.getAvatarURL());
 		builder.withTitle(mail.getContent().getSubject());
 		builder.withDesc(mail.getContent().getBody());
+		builder.withColor(Util.getColor(user, channel));
 
-		if (mail.getGift() != null) {
+		if (mail.getGift() != null && !mail.isGiftCollected()) {
 			Mail.Gift gift = mail.getGift();
 			String giftField = "";
 			if (gift.getCrystals() > 0)
@@ -86,6 +104,33 @@ public class MailCommand implements Command {
 		return builder.build();
 	}
 
+	private void discardAll(IChannel channel, UserInfo info) {
+		IUser user = info.getUser();
+		List<UserMail> mail = info.getMail().stream()
+				.filter(m -> !m.hasGift() || m.isGiftCollected())
+				.collect(Collectors.toList());
+
+		if (mail.isEmpty())
+			Util.sendError(channel, user, "you do not have any deletable mail.");
+		else {
+			info.getMail().removeAll(mail);
+			instance.getMongo().saveUser(info);
+			Bufferer.sendMessage(channel, Util.nameThenID(user) + " discarded all their deletable mail.");
+		}
+	}
+
+	private void discard(IChannel channel, UserInfo info, UserMail mail) {
+		IUser user = info.getUser();
+		if (mail.hasGift() && !mail.isGiftCollected())
+			Util.sendError(channel, user, "you cannot discard mail with pending gift(s).");
+		else {
+			info.getMail().remove(mail);
+			instance.getMongo().saveUser(info);
+			Bufferer.sendMessage(channel, Util.nameThenID(user) + " discarded **"
+					+ mail.getContent().getSubject() + "**.");
+		}
+	}
+
 	private void collectAllGifts(IChannel channel, UserInfo info) {
 		IUser user = info.getUser();
 		if (info.getMail().stream().allMatch(mail -> !mail.hasGift() || mail.isGiftCollected()))
@@ -93,7 +138,7 @@ public class MailCommand implements Command {
 		else {
 			info.getMail().forEach(mail -> mail.collectGift(info));
 			instance.getMongo().saveUser(info);
-			Bufferer.sendMessage(channel, Util.nameThenID(user) + " collected all their mail gifts.");
+			Bufferer.sendMessage(channel, Util.nameThenID(user) + " collected all their gifts.");
 		}
 	}
 
@@ -106,7 +151,8 @@ public class MailCommand implements Command {
 		else {
 			mail.collectGift(info);
 			instance.getMongo().saveUser(info);
-			Bufferer.sendMessage(channel, Util.nameThenID(user) + " collected a mail gift.");
+			Bufferer.sendMessage(channel, Util.nameThenID(user) + " collected a gift from **"
+					+ mail.getContent().getSubject() + "**.");
 		}
 	}
 }
